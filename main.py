33d5e759f6f4f018ef8e21599f8cde664e22715e
Abdulloh-272 <167763951+Abdulloh-272@users.noreply.github.com>
2024-09-24 21:03:45 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+
+matn = "Hello Python"
+
+matn_lower = matn.lower()
+matn_upper = matn.upper()
+matn_capitalize = matn.capitalize()
+matn_strip = matn.strip()
+matn_title = matn.title()
+
+print(f"Lower: {matn_lower}")
+
+print(f"Upper: {matn_upper}")
+print(f"Capitalize: {matn_capitalize} ")
+print(f"Strip: {matn_strip}")
+print(f"Title: {matn_title}")
